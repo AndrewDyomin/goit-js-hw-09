46d538b2b4b32e597b973a0f560c5829f54e0e04
AndrewDyomin <dyomin.andrew1@gmail.com>
2023-06-28 21:47:41 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+const randomColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = randomColor();
+    console.log('started');
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled', '');
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
