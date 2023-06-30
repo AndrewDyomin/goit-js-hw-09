8bd6d9afdd3685f47682d41be08d8d9fe6a46408
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2023-06-30 21:09:44 +0000

    Deploying to gh-pages from @ AndrewDyomin/goit-js-hw-09@3e87914bcfd453992382c47a9701d1f75763ef05 🚀

--- 03-promises.49ca3d0a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("click",void Math.random());
-//# sourceMappingURL=03-promises.49ca3d0a.js.map
--- 03-promises.d895dbf5.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("click",void Math.random());
-//# sourceMappingURL=03-promises.d895dbf5.js.map
