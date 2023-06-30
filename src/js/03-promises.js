3e87914bcfd453992382c47a9701d1f75763ef05
AndrewDyomin <dyomin.andrew1@gmail.com>
2023-07-01 00:07:14 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,39 @@
-const buttonEl = document.querySelector('button');
+import Notiflix from 'notiflix';
 
-buttonEl.addEventListener('click', createPromise());
+const formEl = document.querySelector('.form');
+const inputArr = document.querySelectorAll('input');
+const [ delay, step, amount ] = inputArr;
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+formEl.addEventListener('submit', createPromise);
+
+let position = 1;
+let timerId = null;
+
+function createPromise(event) {
+  event.preventDefault();
+    setTimeout(() => {
+      timerId = setInterval(() => {
+        const shouldResolve = Math.random() > 0.3;
+        const promise = new Promise((resolve, reject) => {
+          if (shouldResolve) {
+            resolve({ position, delay });
+          } else {
+            reject({ position, delay });
+          }
+        });
+        if (position < amount.value) {
+          position ++;
+        } else {
+          clearInterval(timerId);
+        }
+        promise
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay.value}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay.value}ms`);
+        });
+      }, step.value)
+  }, delay.value)
+};
+
